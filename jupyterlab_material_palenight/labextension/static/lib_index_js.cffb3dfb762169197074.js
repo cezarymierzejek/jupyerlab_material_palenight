a6e32df9e2c12760aa2d6e498ff34e14a0f729d7
cezarymierzejek <cezarymierzejek@gmail.com>
2021-01-25 20:21:44 -0500

    updates

---
+++ jupyterlab_material_palenight/labextension/static/lib_index_js.cffb3dfb762169197074.js
@@ -0,0 +1,42 @@
+(self["webpackChunkjupyterlab_palenight"] = self["webpackChunkjupyterlab_palenight"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the jupyterlab_palenight extension.
+ */
+const extension = {
+    id: 'jupyterlab_palenight',
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    autoStart: true,
+    activate: (app, manager) => {
+        console.log('JupyterLab extension jupyterlab_palenight is activated!');
+        const style = 'jupyterlab_palenight/index.css';
+        manager.register({
+            name: 'Jupyterlab Material Palenight',
+            isLight: false,
+            themeScrollbars: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (extension);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.cffb3dfb762169197074.js.map
